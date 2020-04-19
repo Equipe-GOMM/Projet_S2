ace5864921903dbcd5bef89b80351c653565cc6b
jacques.masson <jacques.masson@epita.fr>
2020-04-19 23:51:24 +0200

    Jacques Début lvl air

---
+++ Niveau_Aerien_Jacques/Assets/MovePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    
+    void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        MoveV(verticalMove);
+        MoveH(horizontalMove);
+    }
+
+    void MoveH(float horizontalMove)
+    {
+        Vector3 targetVelocity = new Vector2(horizontalMove,rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+
+    void MoveV(float verticalMove)
+    {
+        Vector3 targetVelocity= new Vector2(rb.velocity.x,verticalMove);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
